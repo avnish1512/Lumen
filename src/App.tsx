3e4b1435644322e42bf0e854925891a14b3371dc
avnish1512 <avnish1512@users.noreply.github.com>
2026-09-19 20:44:00 +0530

    Fix: watch page vertical line remove 2

--- src/App.tsx
+++ src/App.tsx
@@ -10092,7 +10092,7 @@ function WatchScreen({
     <section className="screen watch-screen anime-watch-screen">
       <DetailTopBar onBack={onBack} dark />
 
-      <div className={`anime-watch-main-grid${!hasEpisodes ? ' no-sidebar' : ''}`}>
+      <div className="anime-watch-main-grid">
         {/* LEFT COLUMN: Player -> Under-iframe Bar -> Control & Server bar -> Title/Synopsis/Metadata */}
         <div className="anime-watch-left-col">
           {renderPlayerSection()}
@@ -10319,6 +10319,8 @@ function WatchScreen({
             <Metadata movie={movie} />
           </div>
 
+          {hasEpisodes && renderEpisodePanel(false)}
+
           {relatedList.length > 0 && (
             <div className="watch-similars-rail-section">
               <DetailPosterRail
@@ -10331,13 +10333,6 @@ function WatchScreen({
 
           {renderCommentsSection()}
         </div>
-
-        {/* RIGHT COLUMN: Sidebar */}
-        {hasEpisodes && (
-          <div className="anime-watch-right-col">
-            {renderEpisodePanel(true)}
-          </div>
-        )}
       </div>
 
       {isWatchDownloadModalOpen && (
